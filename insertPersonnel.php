954206289419ff91b3c917734c7be2130350af33
Ali Elgin <vghost@gmail.com>
2013-02-22 12:32:02 +0200

    insert

--- insertPersonnel.php
+++ insertPersonnel.php
@@ -3,8 +3,7 @@
 include 'connect.php'
 	
 	//To be fixed
-	$sqlInsert  = "INSERT INTO tblCalisan (ad, soyad, initials, girisTarihi, sonTerfi ,kimlikNo) 
-	VALUES ('$_POST[ad]', '$_POST[soyad]', '$_POST[initials]', '$_POST[kgTarihi]', '$_POST[sTTarihi]')";
+	$sqlInsert  = "INSERT INTO tblCalisan (ad, soyad, initials, girisTarihi, sonTerfi ,kimlikNo) VALUES ({$_POST['ad']}, {$_POST['soyad']}, {$_POST['initials']}, {$_POST['kgTarihi']}, {$_POST['sTTarihi']})";
 
 	if ($con->query($sqlInsert) == TRUE)
   	{
@@ -13,6 +12,7 @@ include 'connect.php'
 	else
 	{
 		echo 'Error: ' . $con->error;
+		exit();
 	}
 
 	$con->close();
